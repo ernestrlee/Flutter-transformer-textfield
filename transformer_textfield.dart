16c8d3f83ae4bdf6d6328b6febabc7be32ce7d4a
Ernest Lee <30732731+ernestrlee@users.noreply.github.com>
2021-01-23 19:27:26 -0800

    Added dismissible widget

--- transformer_textfield.dart
+++ transformer_textfield.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 class TransformerTextField extends StatefulWidget {
+  final int index;
   final String text;
-  final Function transformFunction;
-  final Function updateFunction;
+  final Function onTransform;
+  final Function onUpdate;
+  final Function onDelete;
 
   TransformerTextField(
-      {this.text, this.transformFunction, this.updateFunction});
+      {this.index, this.text, this.onTransform, this.onUpdate, this.onDelete});
 
   @override
   _TransformerTextFieldState createState() => _TransformerTextFieldState();
@@ -14,14 +16,13 @@ class TransformerTextField extends StatefulWidget {
 
 class _TransformerTextFieldState extends State<TransformerTextField> {
   bool isEnabled = false;
-  TextEditingController _controller;
+  TextEditingController _controller = TextEditingController();
   FocusNode _focusNode;
 
   @override
   void initState() {
     super.initState();
-    _controller = TextEditingController(text: widget.text);
-    _controller.addListener(updateText);
+    _controller.text = widget.text;
     _focusNode = FocusNode();
     _focusNode.addListener(_onFocusChange);
   }
@@ -29,6 +30,7 @@ class _TransformerTextFieldState extends State<TransformerTextField> {
   @override
   void dispose() {
     _controller.dispose();
+    _focusNode.removeListener(_onFocusChange);
     _focusNode.dispose();
     super.dispose();
   }
@@ -40,59 +42,84 @@ class _TransformerTextFieldState extends State<TransformerTextField> {
         isEnabled = true;
         // Populate the texfield with the text
         _controller.text = widget.text;
+        // Start cursor at end of text when tapped on
+        _controller.selection = TextSelection.fromPosition(
+            TextPosition(offset: _controller.text.length));
       });
     } else {
+      // Update the original text value
+      _updateText();
       setState(() {
-        // Update the original text value
-        widget.updateFunction(_controller.text);
         // Toggle to the text view
         isEnabled = false;
       });
     }
   }
 
-  void updateText() {
-    widget.updateFunction(_controller.text);
+  void _updateText() {
+    widget.onUpdate(widget.index, _controller.text);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(10.0),
-      alignment: Alignment.centerLeft,
-      constraints: BoxConstraints(
-        minHeight: 50.0,
-      ),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.black),
-      ),
-      child: isEnabled == true
-          ? Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: InputBorder.none,
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isEnabled = true;
+          _focusNode.requestFocus();
+        });
+      },
+      child: Container(
+        child: isEnabled == true
+            ? Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                  focusNode: _focusNode,
+                  controller: _controller,
+                  autofocus: true,
                 ),
-                focusNode: _focusNode,
-                controller: _controller,
-                autofocus: isEnabled == true ? true : false,
-              ),
-            )
-          : GestureDetector(
-              onTap: () {
-                setState(() {
-                  isEnabled = true;
-                  _focusNode.requestFocus();
-                });
-              },
-              child: Container(
-                width: double.infinity,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(widget.transformFunction(widget.text)),
+              )
+            : Dismissible(
+                key: UniqueKey(),
+                onDismissed: (direction) {
+                  widget.onDelete(widget.index);
+                },
+                background: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        'Delete',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                direction: DismissDirection.endToStart,
+                child: Center(
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    constraints: BoxConstraints(
+                      minHeight: 50,
+                    ),
+                    width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(widget.onTransform(_controller.text)),
+                    ),
+                  ),
                 ),
               ),
-            ),
+      ),
     );
   }
 }
